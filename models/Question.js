d379540f56b0c09cb0eb14a4733ba4d2f9bc7d10
Chely Ho <adept_inept@yahoo.com>
2017-11-16 23:25:10 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -3,8 +3,19 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable() {
+    const sql =`
+    CREATE TABLE IF NOT EXISTS questions (
+      id INTEGER PRIMARY KEY,
+      content TEXT
+    )
+    `
+    return new Promise(function(resolve){
+      db.run(sql, function(){
+        resolve('Database is finished.')
+      })
+     })
+  }
 }
-
 
 module.exports = Question;
