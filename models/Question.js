b4156c6f2c26cba4a8406169f462f90ca98c5eb4
Chely Ho <adept_inept@yahoo.com>
2017-11-17 13:15:43 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -4,14 +4,14 @@ const db = require("../config/db")
 
 class Question{
   static CreateTable() {
-  //lab passes with table creation above the return Promise. Solution says to put it within Promise. is there a difference?
+    const sql =`                  
+    CREATE TABLE questions (
+      id INTEGER PRIMARY KEY,
+      content TEXT
+    )
+    `             //Solution says to put CreateTable within Promise. is there much of a difference?
+
     return new Promise(function(resolve){
-      const sql =`
-      CREATE TABLE questions (
-        id INTEGER PRIMARY KEY,
-        content TEXT
-      )
-      `
       db.run(sql, function(){
         resolve('boop')
       })
